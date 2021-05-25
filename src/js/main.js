407e2046516d762371ea15181c927ccf5cef0574
kevin calle <kevinm19@gmail.com>
2021-05-25 02:13:35 -0500

    compuaudios2

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,36 @@
-$(function(){
-
+$(document).ready(function(){
+    
+    
     
     $('.items').slick({
-        dots: true,
-        infinite: true,
-        speed: 800,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        responsive: [
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      responsive: [
         {
-        breakpoint: 1024,
-        settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-        }
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
         },
         {
-        breakpoint: 600,
-        settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-        }
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
         },
         {
-        breakpoint: 480,
-        settings: {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
             slidesToScroll: 1
+          }
         }
-        }
-        
         ]
         });
               
